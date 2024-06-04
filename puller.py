c7f953061f8227b7b82d6a2040e5fa4868348143
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-06-04 22:51:50 +0300

    Added CLI tool for efficient model downloading

--- puller.py
+++ puller.py
@@ -1,7 +1,11 @@
 import requests
+import argparse
+import os
 
 
 def download_file(link, filename):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
     response = requests.get(link)
 
     if response.status_code == 200:
@@ -12,7 +16,25 @@ def download_file(link, filename):
         print('Failed to download')
 
 
-url = "https://raw.githubusercontent.com/daniil-vikulov/Hate-Speech-Detection/main/models/model_catboost"
-filename = "model_catboost"
+def main(model_type):
+    base_link = "https://raw.githubusercontent.com/daniil-vikulov/Hate-Speech-Detection/main/models/"
 
-download_file(url, filename)
+    if model_type == "all":
+        model_types = ["catboost", "logisticregression", "randomforest", "vectorizer"]
+    else:
+        model_types = [model_type]
+
+    for mt in model_types:
+        link = f"{base_link}model_{mt}"
+        fn = f"model/model_{mt}"
+        download_file(link, fn)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download model")
+    parser.add_argument("model_type", choices=['catboost', 'randomforest', 'logisticregression', "vectorizer", 'all'],
+                        help="Specify model name to pull")
+
+    args = parser.parse_args()
+
+    main(args.model_type)
