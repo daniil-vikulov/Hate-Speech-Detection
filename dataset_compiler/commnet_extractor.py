4a7030f5ccbdcca4509c8fd3d00da131cfcab7ee
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-06-05 23:39:40 +0300

    Updated requirements.txt Added comment parser (select and copy comment section from the page) Added .json with dataset (2500+ comments)

---
+++ dataset_compiler/commnet_extractor.py
@@ -0,0 +1,64 @@
+import os
+import re
+import chardet
+import json
+
+
+def save(str_list: list):
+    with open('comments.json', 'w', encoding='utf-8') as file:
+        json.dump(str_list, file, ensure_ascii=False, indent=4)
+
+
+def load() -> []:
+    with open('comments.json', 'r', encoding='utf-8') as file:
+        lst = json.load(file)
+    return lst
+
+
+def get_comments(filename):
+    with open(filename, 'rb') as fl:
+        raw_data = fl.read()
+        encoding = chardet.detect(raw_data)['encoding']
+
+    with open(filename, 'r', encoding=encoding) as fl:
+        raw_data = fl.read()
+
+    lines = [line.strip() for line in raw_data.split('\n') if line.strip()]
+
+    filters = [
+        r'^\d+[Kk]?$',
+        r'^Reply$',
+        r'^\d+\sreplies$',
+        r'^\d+\s+reply$',
+        r'^@\w+',
+        r'^\d+\s+(year|month|week|day|hour|minute)s?\s+ago$',
+        r'^\d+\s+reply,\s+\d+\s+(year|month|week|day|hour|minute)s?\s+ago$',
+        r'^\d+\s+(year|month|week|day|hour|minute)s?\s+ago\s+\(edited\)$'
+    ]
+
+    filter_regex = re.compile('|'.join(filters))
+
+    comments = []
+
+    for line in lines:
+        if not filter_regex.search(line):
+            comments.append(line)
+
+    return comments
+
+
+files = os.listdir('raw')
+file_list = [file for file in files if os.path.isfile(os.path.join('raw', file))]
+
+data = []
+
+for f in files:
+    print(f)
+    data.extend(get_comments(f'raw/{f}'))
+
+save(data)
+dt = load()
+
+print(len(dt))
+print(len(data))
+
